fbe2a29b527f27e63bf519952921a64447c51716
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-06-29 16:27:59 +0530

    abi and bytecode

--- compile.js
+++ compile.js
@@ -8,3 +8,25 @@ let web3 = new Web3("http://127.0.0.1:7545");
 
 let fileContent = fs.readFileSync("methodscall.sol").toString();
 
+let input = {
+  language: "Solidity",
+  sources: {
+    "methodscall.sol": {
+      content: fileContent,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+};
+
+let output = JSON.parse(solc.compile(JSON.stringify(input)));
+console.log(output);
+ABI = output.contracts["methodscall.sol"]["methodcall"].abi;
+bytecode = output.contracts["methodscall.sol"]["methodscall"].evm.bytecode.object;
+console.log("abi :", ABI);
+console.log("bytecode :", bytecode);
